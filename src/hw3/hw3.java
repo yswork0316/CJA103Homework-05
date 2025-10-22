2785a2dab7aecc6bc1e805732fb62c2de1dce1c2
ys Hsu <despair333@gmail.com>
2025-10-22 23:47:05 +0800

    hw3 Q1 complete

---
+++ src/hw3/hw3.java
@@ -0,0 +1,72 @@
+package hw3;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+//1.請設計一隻程式，使用者輸入三個數字後，輸出結果會為正三角形、等腰三角形、其它三角形或不是三角形，如圖示結果：
+//(進階功能：加入直角三角形的判斷) 
+
+//public class hw3 {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int[] t = new int[3];
+//		System.out.println("請輸入三個整數:");
+//		
+//		for(int i=0; i<3; i++) {
+//			t[i] = sc.nextInt(); 
+//		}
+//		
+//		Arrays.sort(t);
+//		
+//		if((t[0]+t[1]) > t[2]) {
+//			if((t[0] == t[1]) && t[0]== t[2]) {
+//				System.out.println("正三角形");
+//			}
+//			else if(t[0] == t[1] || t[0] == t[2] || t[1] == t[2]){
+//				System.out.println("等腰三角形");
+//			}
+//			else {
+//				System.out.println("其他三角形");
+//			}
+//			
+//			if(t[0]*t[0] + t[1]*t[1] == t[2]*t[2]) {
+//				System.out.println("也是直角三角形");     
+//			}
+//		}
+//		else {
+//			System.out.println("不是三角形");
+//		}
+//	}
+//}
+
+//================================================================================
+
+//2.請設計一隻程式，會亂數產生一個0～9的數字，然後可以玩猜數字遊戲，猜錯會顯示錯誤訊息，猜對則顯示正確訊息，如圖示結果：
+//(進階功能：產生0～100亂數，每次猜就會提示你是大於還是小於正確答案)
+
+public class hw3 {
+	public static void main(String[] args) {
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
