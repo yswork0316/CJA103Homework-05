8038b21ce2a99236a7dd223e80dc92b4bf4f9a21
ys Hsu <despair333@gmail.com>
2025-10-23 00:36:00 +0800

    hw3 Q3 complete

--- src/hw3/hw3.java
+++ src/hw3/hw3.java
@@ -44,29 +44,48 @@ import java.util.Scanner;
 //2.請設計一隻程式，會亂數產生一個0～9的數字，然後可以玩猜數字遊戲，猜錯會顯示錯誤訊息，猜對則顯示正確訊息，如圖示結果：
 //(進階功能：產生0～100亂數，每次猜就會提示你是大於還是小於正確答案)
 
+//public class hw3 {
+//	public static void main(String[] args) {
+//		System.out.println("開始猜數字吧!(0~100)");
+//		Scanner sc = new Scanner(System.in);
+//		int n = (int) (Math.random()*101);
+//		int A = sc.nextInt();
+//		
+//		while (A != n) {
+//			System.out.println("猜錯囉");
+//			if(A > n) {
+//				System.out.println("大於正確答案");
+//			}
+//			else {
+//				System.out.println("小於正確答案");
+//			}
+//			
+//			A = sc.nextInt();
+//		}
+//		System.out.println("答對了!答案就是" + A);
+//	}
+//}
+
+//================================================================================
+/*3.阿文很喜歡簽大樂透(1～49)，但他是個善變的人，上次討厭數字是4，但這次他想要依心情決定討
+厭哪個數字，請您設計一隻程式，讓阿文可以輸入他不想要的數字(1～9)，畫面會顯示他可以選擇
+的號碼與總數，如圖： (進階挑戰：輸入不要的數字後，直接亂數印出6個號碼且不得重複)*/
 public class hw3 {
 	public static void main(String[] args) {
-		System.out.println("開始猜數字吧!(0~100)");
+		System.out.println("阿文...請輸入你討厭哪個數字?");
 		Scanner sc = new Scanner(System.in);
-		int n = (int) (Math.random()*101);
-		int A = sc.nextInt();
+		int hatenumber = sc.nextInt();
 		
-		while (A != n) {
-			System.out.println("猜錯囉");
-			if(A > n) {
-				System.out.println("大於正確答案");
+		for(int i = 1; i <= 49; i++) {
+		if(i % 10 == hatenumber || (i >= hatenumber*10 && i < (hatenumber*10+10))) {
+			continue;
 			}
-			else {
-				System.out.println("小於正確答案");
-			}
-			
-			A = sc.nextInt();
+		
+		System.out.print(i + " ");
 		}
-		System.out.println("答對了!答案就是" + A);
 	}
 }
 
-//================================================================================
 
 
 
